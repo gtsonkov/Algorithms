5d3179bee1cdff855a4b27569d3036d76341e368
gtsonkov <gtsonkov@abv.bg>
2019-05-12 16:00:34 +0200

    Add Generating Combinations Algorithm

---
+++ GeneratingCombinations/GeneratingCombinations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace GeneratingCombinations
+{
+    class GeneratingCombinations
+    {
+        static int[] SetOfNums;
+        static void Main(string[] args)
+        {
+            int boarder = -1;
+            int[] vector = new int[2];
+            SetOfNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            GenerateCombinations(SetOfNums,vector,boarder,0);
+        }
+
+        static void GenerateCombinations(int[] SetOfNUms, int[] vector, int boarder, int Index)
+        {
+            if (Index >= vector.Length)
+            {
+                Console.WriteLine(string.Join(" ",vector));
+            }
+            else
+            {
+                for (int i = boarder+1; i < SetOfNUms.Length; i++)
+                {
+                    vector[Index] = SetOfNUms[i];
+                    GenerateCombinations(SetOfNUms, vector, i, Index +1);
+                }
+            }
+        }
+    }
+}
