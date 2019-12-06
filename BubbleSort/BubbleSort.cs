73cab1fa2f05b1b244a6cbe787cd917249176d25
Georgi Tsonkov <gtsonkov@abv.bg>
2019-12-06 21:21:45 +0100

    BubbleSort

--- BubbleSort/BubbleSort.cs
+++ BubbleSort/BubbleSort.cs
@@ -9,31 +9,75 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
-            int[] unsortedArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int[] sortedArr = SortArray(unsortedArr);
-            Console.Write("Create .txt file? (y/n)");
-            string command = Console.ReadLine();
-            if (command == "y")
+            Console.WriteLine("Read data from: ");
+            Console.WriteLine("0 - Console");
+            Console.WriteLine("1 - File");
+            string readFromCommand = Console.ReadLine();
+
+            if (readFromCommand == "0")
             {
-                using (StreamWriter writer = new StreamWriter("endArray.txt"))
+                int[] unsortedArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int[] sortedArr = SortArray(unsortedArr);
+                Console.Write("Create .txt file? (y/n)");
+                string saveCommand = Console.ReadLine();
+
+                if (saveCommand == "y")
                 {
-                    StringBuilder textBuilder = new StringBuilder();
-                    textBuilder.AppendJoin(";", sortedArr).AppendLine();
-                    writer.Write(textBuilder.ToString());
+
+                    SaveToText(sortedArr);
+                }
+                else
+                {
+                    PrintToConsole(sortedArr);
                 }
             }
             else
             {
-                Console.WriteLine(string.Join(" ", sortedArr));
+                Console.Write("File path:");
+                string filePath = Console.ReadLine();
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    int[] unsortedArr = reader.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                    reader.Close();
+                    int[] sortedArr = SortArray(unsortedArr);
+                    Console.Write("Create .txt file? (y/n)");
+                    string saveCommand = Console.ReadLine();
+                    if (saveCommand == "y")
+                    {
+                        SaveToText(sortedArr);
+                    }
+                    else
+                    {
+                        PrintToConsole(sortedArr);
+                    }
+                }
+            }
+        }
+
+        private static void PrintToConsole(int[] sortedArr)
+        {
+            Console.WriteLine(string.Join(" ", sortedArr));
+        }
+
+        private static void SaveToText(int[] sortedArr)
+        {
+            using (StreamWriter writer = new StreamWriter("endArray.txt"))
+            {
+                StringBuilder textBuilder = new StringBuilder();
+                foreach (var number in sortedArr)
+                {
+                    textBuilder.AppendLine(number.ToString());
+                }
+                writer.Write(textBuilder.ToString());
             }
         }
 
         private static int[] SortArray(int[] unsortedArr)
         {
             int count = unsortedArr.Length;
-            for (int i = 0; i < count -1; i++)
+            for (int i = 0; i < count; i++)
             {
-                for (int j = 0; j < count-i - 1; j++)
+                for (int j = 0; j < count - 1; j++)
                 {
                     if (unsortedArr[j] > unsortedArr[j+1])
                     {
